16c7df47bc327d49a9ea0a5b7feacf7fd9566e83
unknown <clintonotuya66@gmail.com>
2025-05-10 14:17:14 +0300

    got started with templates and views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    number_of_books = Book.objects.all().count()
+    number_of_authors = Author.objects.count()
+    number_instances_available = BookInstance.objects.filter(status__exact='A').count()
+    number_instances= BookInstance.objects.all().count()
+    # Increment the number of visits in the session	
+    if 'number_of_visits' in request.session:
+        request.session['number_of_visits'] += 1
+    else:
+        request.session['number_of_visits'] = 1
+	# Get the number of visits from the session
+ 
+ 
+    number_of_visits = request.session['number_of_visits']
+	
+
+
+
+    context = {
+		'number_of_books': number_of_books,
+		'number_of_authors': number_of_authors,
+		'number_instances_available': number_instances_available,
+		'number_of_visits': number_of_visits,
+		'number_instances': number_instances,
+	}
+    return render(request, 'catalog/index.html',context=context)
+
+
+
+
+
+def about(request):
+    return render(request, 'catalog/about.html')
